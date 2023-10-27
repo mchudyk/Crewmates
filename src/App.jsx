cf5799fcd51fae84fdeea181c6e83e5590be4119
mchudyk <123168391+mchudyk@users.noreply.github.com>
2023-10-26 22:58:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import CrewmateDetail from "./components/CrewmateDetail";
 import './App.css';
 
 const URL = 'https://wcllvdzpqaxvqrjcpati.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjbGx2ZHpwcWF4dnFyamNwYXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzNTE3ODMsImV4cCI6MjAxMzkyNzc4M30.gULt3u7vTpzVFedYMzJbt8CvRI3yO55rjhQ2r2jtnCQ';
+const API_KEY = 'ooopoppssss';
 
 export const supabase = createClient(URL, API_KEY);
 
